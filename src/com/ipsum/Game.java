3115576d31827cfcf88497c9b17361161c49d116
Naxanria <gerbrandnijk@gmail.com>
2014-05-02 11:19:38 +0200

    init commit

---
+++ src/com/ipsum/Game.java
@@ -0,0 +1,20 @@
+package com.ipsum;
+
+import java.awt.*;
+
+public class Game extends Canvas
+{
+
+
+	public Game()
+	{
+		System.out.println("No hello world!");
+	}
+
+
+
+	public static void main(String[] args)
+	{
+		Game game = new Game();
+	}
+}
